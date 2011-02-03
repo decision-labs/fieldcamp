3bf6afcedb29186a1e4d89e6990319ad1be62347
Kashif Rasul <kashif@nomad-labs.com>
2011-02-03 12:35:46 +0100

    Installed mobile-fu plugin.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  has_mobile_fu
   before_filter :set_locale
+  
   def set_locale
     # if params[:locale] is nil then I18n.default_locale will be used
     I18n.locale = params[:locale] || I18n.default_locale

bf580a63aa0fb025cea35a2f112094c05d972235
Shoaib Burq <shoaib@nomad-labs.com>
2011-02-11 00:23:19 +0100

    new event geom is being created.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -36,7 +36,7 @@ class EventsController < ApplicationController
 
     respond_to do |format|
       format.html
-      format.xml  { render :xml => @event }
+      format.mobile
     end
   end
 
@@ -49,45 +49,47 @@ class EventsController < ApplicationController
   # POST /events.xml
   def create
     @project = Project.find(params[:project_id])
+    wkt = params[:event].delete(:geom)
     @event = @project.events.build(params[:event])
+    @event.geom = Point.from_ewkt(wkt)
 
     respond_to do |format|
       if @event.save
         format.html { redirect_to(@project, :notice => 'Event was successfully created.') }
-        format.xml  { render :xml => @event, :status => :created, :location => @event }
+        format.mobile { redirect_to(@project) }
       else
         format.html { render :action => "new" }
-        format.xml  { render :xml => @event.errors, :status => :unprocessable_entity }
+        format.mobile  { render :action => "new" }
       end
     end
   end
 
   # PUT /events/1
   # PUT /events/1.xml
-  def update
-    @project = Project.find(params[:project_id])
-    @event = @project.events.find(params[:id])
-
-    respond_to do |format|
-      if @event.update_attributes(params[:event])
-        format.html { redirect_to(@event, :notice => 'Event was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @event.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
+  # def update
+  #   @project = Project.find(params[:project_id])
+  #   @event = @project.events.find(params[:id])
+  # 
+  #   respond_to do |format|
+  #     if @event.update_attributes(params[:event])
+  #       format.html { redirect_to(@event, :notice => 'Event was successfully updated.') }
+  #       format.xml  { head :ok }
+  #     else
+  #       format.html { render :action => "edit" }
+  #       format.xml  { render :xml => @event.errors, :status => :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # DELETE /events/1
   # DELETE /events/1.xml
-  def destroy
-    @event = Event.find(params[:id])
-    @event.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(events_url) }
-      format.xml  { head :ok }
-    end
-  end
+  # def destroy
+  #   @event = Event.find(params[:id])
+  #   @event.destroy
+  # 
+  #   respond_to do |format|
+  #     format.html { redirect_to(events_url) }
+  #     format.xml  { head :ok }
+  #   end
+  # end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,27 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+// TODO: move to  maps.js
+
+$(document).ready(function() {
+  // body...
+})
+
+function getElevationForLatLon(lat,lon) {
+  var elevator = new google.maps.ElevationService();
+  var positionalRequest = {
+    'locations': new google.maps.LatLng(lat,lon)
+  }
+  elevator.getElevationForLocations(positionalRequest, function(results, status) {
+    if (status == google.maps.ElevationStatus.OK) {
+      // Retrieve the first result
+      if (results[0]) {
+        return results[0].elevation;
+      }else{
+        return null;
+      }
+    }else{
+      return null;
+    }
+  }) // elevator callback ends
+}

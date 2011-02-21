ece2ae075f39e7e0d3ae1290a39ca354bfc0a803
Kashif Rasul <kashif@nomad-labs.com>
2011-02-21 12:57:13 +0100

    Added locales for projects/events.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
 
-  def link_to_icon(icon_name, url_or_object, options={})
+  def link_to_icon(icon_name, icon_title, url_or_object, options={})
     options.merge!({ :class => "icon #{icon_name}" })
 
-    link_to(image_tag("icons/#{icon_name}.png", { :title => icon_name }),
+    link_to(image_tag("icons/#{icon_name}.png", { :title => icon_title }),
                       url_or_object,
                       options)
   end
--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -2,10 +2,10 @@ module ProjectsHelper
   def project_title_links(project)
     content_tag :h1 do
       [ project.title,
-        link_to_icon('show', project),
-        link_to_icon('edit', edit_project_path(project)),
-        link_to_icon('destroy', project, {
-          :confirm => 'Are you sure?',
+        link_to_icon('show', t(:show), project),
+        link_to_icon('edit', t(:edit), edit_project_path(project)),
+        link_to_icon('destroy', t(:destroy), project, {
+          :confirm => t(:are_you_sure),
           :method => :delete
         })
       ].join(' ').html_safe

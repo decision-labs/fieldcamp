a63445fffd4718defd3b2e67ec34ba6c3ff6f07b
Kashif Rasul <kashif@nomad-labs.com>
2011-02-21 16:51:35 +0100

    Removed dead code.

--- public/javascripts/events.js
+++
@@ -1,9 +0,0 @@
-// function json2html (json) {
-//   var ctr = $("<div/>", {"class":"info_windown"});
-//   $.each(json, function (k, v) {
-//     var attr = $("<div/>", {"class":k});
-//     attr.add("p").html(k + ": " + v);
-//     ctr.append(attr);
-//   });
-//   return ctr.html();
-// }

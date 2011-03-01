f638dbd645754aa0017ec60f12b6ceaf34015115
Kashif Rasul <kashif@nomad-labs.com>
2011-03-01 14:39:06 +0100

    Refactoring footer.mobile links to disable the current page link. Added locations/show.mobile

--- app/helpers/mobile_helper.rb
+++ app/helpers/mobile_helper.rb
@@ -1,15 +1,29 @@
 module MobileHelper
 
-  def mobile_rel_external(model_name)
-    if model_name.singularize != controller.controller_name.singularize
+  def mobile_footer_link(model_symbol)
+    href = '#'
+    href = send("#{model_symbol.to_s.pluralize}_path") unless
+      model_symbol.to_s.singularize == controller.controller_name.singularize
+    klass = mobile_active_button_class(model_symbol)
+    rel = mobile_rel_external(model_symbol)
+
+    content_tag :a, {:class => klass, :rel => rel, :href => href} do
+      t(model_symbol)
+    end
+  end
+
+  private
+
+  def mobile_rel_external(model_symbol)
+    if model_symbol.to_s.singularize != controller.controller_name.singularize
       return 'external'
     else
       return ''
     end
   end
 
-  def mobile_active_button_class(model_name)
-    if model_name.singularize == controller.controller_name.singularize
+  def mobile_active_button_class(model_symbol)
+    if model_symbol.to_s.singularize == controller.controller_name.singularize
       return 'ui-btn-active' 
     else 
       return ''

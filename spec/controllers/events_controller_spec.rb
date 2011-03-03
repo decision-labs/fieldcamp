01365e10b8c473850019bdf4cdcc69e84c1939cf
Shoaib Burq <shoaib@nomad-labs.com>
2011-03-03 12:35:23 +0100

    Started testing events_controller.

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -7,119 +7,104 @@ require 'spec_helper'
 describe EventsController do
 
   def mock_event(stubs={})
-    @mock_event ||= mock_model(Event, stubs).as_null_object
+    @mock_event ||= mock_model(Event, stubs)
   end
 
-  describe "GET index" do
-    it "assigns all events as @events" do
-      Event.stub(:all) { [mock_event] }
-      get :index
-      assigns(:events).should eq([mock_event])
+  def mock_project(stubs={})
+    @mock_project ||= mock_model(Project, stubs)
+  end
+
+  def mock_admin_user(stubs={})
+    @mock_admin_user ||= mock_model(User, stubs.merge(:role => 'admin'))
+  end
+
+  def mock_publisher_user(stubs={})
+    @mock_publisher_user ||= mock_model(User, stubs.merge(:role => 'publisher'))
+  end
+
+  describe "publisher user" do
+    before(:each) do
+      # sign_in mock_user
+      request.env['warden'] = mock( Warden,
+                                    :authenticate  => mock_publisher_user(:id => "1"),
+                                    :authenticate! => mock_publisher_user(:id => "1"))
     end
-  end
 
-  describe "GET show" do
-    it "assigns the requested event as @event" do
-      Event.stub(:find).with("37") { mock_event }
-      get :show, :id => "37"
-      assigns(:event).should be(mock_event)
+    describe "GET index" do
+      it "assigns all events as @events" do
+        Project.stub(:find).with("1") { mock_project }
+        Project.stub(:find) { mock_project(:events => [mock_event]) }
+
+        get :index, :project_id => "1"
+        assigns(:events).should eq([mock_event])
+        assigns(:project).should be(mock_project)
+      end
     end
-  end
 
-  describe "GET new" do
-    it "assigns a new event as @event" do
-      Event.stub(:new) { mock_event }
-      get :new
-      assigns(:event).should be(mock_event)
+    describe "GET show" do
+      it "assigns the requested event as @event and project as @project" do
+        Project.stub(:find).with("1") { mock_project }
+        mock_project.stub_chain(:events, :find).with("37"){ mock_event }
+
+        get :show, :id => "37", :project_id => "1"
+        assigns(:event).should be(mock_event)
+        assigns(:project).should be(mock_project)
+      end
     end
-  end
 
-  describe "GET edit" do
-    it "assigns the requested event as @event" do
-      Event.stub(:find).with("37") { mock_event }
-      get :edit, :id => "37"
-      assigns(:event).should be(mock_event)
-    end
-  end
+    describe "GET new" do
+      it "assigns new event as @event and requested project as @project" do
+        Project.stub(:find).with("1") { mock_project }
+        Event.stub(:new) { mock_event }
 
-  describe "POST create" do
-    describe "with valid params" do
-      it "assigns a newly created event as @event" do
-        Event.stub(:new).with({'these' => 'params'}) { mock_event(:save => true) }
-        post :create, :event => {'these' => 'params'}
+        get :new, "project_id" => "1", :user_id => "1"
+
+        assigns(:project).should be(mock_project)
         assigns(:event).should be(mock_event)
       end
-
-      it "redirects to the created event" do
-        Event.stub(:new) { mock_event(:save => true) }
-        post :create, :event => {}
-        response.should redirect_to(event_url(mock_event))
-      end
     end
 
-    describe "with invalid params" do
-      it "assigns a newly created but unsaved event as @event" do
-        Event.stub(:new).with({'these' => 'params'}) { mock_event(:save => false) }
-        post :create, :event => {'these' => 'params'}
-        assigns(:event).should be(mock_event)
-      end
-
-      it "re-renders the 'new' template" do
-        Event.stub(:new) { mock_event(:save => false) }
-        post :create, :event => {}
-        response.should render_template("new")
-      end
-    end
-  end
-
-  describe "PUT update" do
-    describe "with valid params" do
-      it "updates the requested event" do
-        Event.stub(:find).with("37") { mock_event }
-        mock_event.should_receive(:update_attributes).with({'these' => 'params'})
-        put :update, :id => "37", :event => {'these' => 'params'}
-      end
-
+    describe "GET edit" do
       it "assigns the requested event as @event" do
-        Event.stub(:find) { mock_event(:update_attributes => true) }
-        put :update, :id => "1"
-        assigns(:event).should be(mock_event)
-      end
+        Project.stub(:find).with("1") { mock_project }
+        mock_project.stub_chain(:events, :find).with("37"){ mock_event }
+        mock_event.stub(:user_id){"1"} # for CanCan Ability.rb
 
-      it "redirects to the event" do
-        Event.stub(:find) { mock_event(:update_attributes => true) }
-        put :update, :id => "1"
-        response.should redirect_to(event_url(mock_event))
+        get :edit, :id => "37", :project_id => "1", :user_id => "1"
+        assigns(:event).should be(mock_event)
+        assigns(:project).should be(mock_project)
       end
     end
 
-    describe "with invalid params" do
-      it "assigns the event as @event" do
-        Event.stub(:find) { mock_event(:update_attributes => false) }
-        put :update, :id => "1"
-        assigns(:event).should be(mock_event)
-      end
-
-      it "re-renders the 'edit' template" do
-        Event.stub(:find) { mock_event(:update_attributes => false) }
-        put :update, :id => "1"
-        response.should render_template("edit")
+    describe "POST create" do
+      describe "with valid params" do
+        it "assigns a newly created event as @event"
+        it "redirects to the project of the created event"
       end
     end
+
+    describe "PUT update" do
+      describe "with valid params" do
+        it "updates the requested event"
+        it "assigns the requested event as @event"
+        it "redirects to the event"
+      end
+
+      describe "with invalid params" do
+        it "assigns the event as @event"
+        it "re-renders the 'edit' template"
+      end
+    end
+
+    describe "DELETE destroy" do
+      it "destroys the requested event"
+      it "redirects to the events list"
+    end
+
   end
 
-  describe "DELETE destroy" do
-    it "destroys the requested event" do
-      Event.stub(:find).with("37") { mock_event }
-      mock_event.should_receive(:destroy)
-      delete :destroy, :id => "37"
-    end
-
-    it "redirects to the events list" do
-      Event.stub(:find) { mock_event }
-      delete :destroy, :id => "1"
-      response.should redirect_to(events_url)
-    end
-  end
+  # TODO:
+  # TODO: add test for admin users
+  # TODO: add test for guest users
 
 end

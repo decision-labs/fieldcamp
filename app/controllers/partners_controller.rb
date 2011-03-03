9d9e5ba31eb8210c28750249dc94fc4b760f23f9
Kashif Rasul <kashif@nomad-labs.com>
2011-03-03 11:15:05 +0100

    Added authorization to locations, partners and sector controllers.

--- app/controllers/partners_controller.rb
+++ app/controllers/partners_controller.rb
@@ -1,83 +1,58 @@
 class PartnersController < ApplicationController
-  # GET /partners
-  # GET /partners.xml
+
+  respond_to :mobile, :html
+
   def index
     @partners = Partner.all(:include => :projects)
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @partners }
-    end
   end
 
-  # GET /partners/1
-  # GET /partners/1.xml
   def show
     @partner = Partner.find(params[:id], :include => :projects)
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @partner }
-    end
   end
 
-  # GET /partners/new
-  # GET /partners/new.xml
   def new
+    authorize! :new, Partner
     @partner = Partner.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @partner }
-    end
   end
 
-  # GET /partners/1/edit
   def edit
+    authorize! :edit, Partner
     @partner = Partner.find(params[:id])
   end
 
-  # POST /partners
-  # POST /partners.xml
   def create
+    authorize! :edit, Partner
     @partner = Partner.new(params[:partner])
 
     respond_to do |format|
       if @partner.save
         format.html { redirect_to(@partner, :notice => 'Partner was successfully created.') }
-        format.xml  { render :xml => @partner, :status => :created, :location => @partner }
       else
         format.html { render :action => "new" }
-        format.xml  { render :xml => @partner.errors, :status => :unprocessable_entity }
       end
     end
   end
 
-  # PUT /partners/1
-  # PUT /partners/1.xml
   def update
+    authorize! :update, Partner
     @partner = Partner.find(params[:id])
 
     respond_to do |format|
       if @partner.update_attributes(params[:partner])
         format.html { redirect_to(@partner, :notice => 'Partner was successfully updated.') }
-        format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
-        format.xml  { render :xml => @partner.errors, :status => :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /partners/1
-  # DELETE /partners/1.xml
   def destroy
+    authorize! :destroy, Partner
     @partner = Partner.find(params[:id])
     @partner.destroy
 
     respond_to do |format|
       format.html { redirect_to(partners_url) }
-      format.xml  { head :ok }
     end
   end
 end

46ad277f061ebb3ba6e68ac86caedc174407a49a
Kashif Rasul <kashif@nomad-labs.com>
2011-03-04 17:38:12 +0100

    Initial commit of web-app-theme. Not finished.

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,16 @@
 module EventsHelper
+  # def model_title_links(m, t="title", header_level=:h2, parent_model = nil)
+  #   path = [parent_model.try(:downcase), m.class.name.downcase, 'path'].compact.join('_')
+  #   content_tag header_level, :class=>'title' do
+  #     [ m.send(t),
+  #       link_to_icon('show', t(:show), m ),
+  #       (can? :update,  m) ? link_to_icon('edit', t(:edit), send(['edit', path].join('_') , m)) : nil,
+  #       (can? :destroy, m) ? link_to_icon('destroy', t(:destroy), m, {
+  #         :confirm => t(:are_you_sure),
+  #         :method => :delete
+  #       }) : nil
+  #     ].compact.join(' ').html_safe
+  #   end
+  # end
+
 end

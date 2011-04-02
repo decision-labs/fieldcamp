eb136146f3bd724851c46322ba4b9c9acbc6cf8a
Kashif Rasul <kashif@nomad-labs.com>
2011-04-02 21:27:29 +0200

    Use reset_counters instead of update_attributes and use find_each instead of each.

--- db/migrate/20110402182403_add_projects_count.rb
+++ db/migrate/20110402182403_add_projects_count.rb
@@ -1,9 +1,10 @@
 class AddProjectsCount < ActiveRecord::Migration
   def self.up
     add_column :locations, :projects_count, :integer, :default => 0
+    
     Location.reset_column_information
-    Location.all.each do |l|
-      l.update_attribute :projects_count, l.projects.length
+    Location.find_each do |l|
+      Location.reset_counters l.id, :projects
     end
   end
 

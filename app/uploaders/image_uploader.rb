34ec9c3d9b672304d5c4e11243b58c3378904f4f
Kashif Rasul <kashif@nomad-labs.com>
2011-06-15 16:01:24 +0200

    added image thumbnails.

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -11,4 +11,13 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  process :resize_to_limit => [640, 640]
+  
+  version :thumb do
+      process :resize_to_limit => [160, 160]
+  end
+  
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 end

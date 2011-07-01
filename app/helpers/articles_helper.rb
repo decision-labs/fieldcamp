3964d0c2796d953941e92610128dcdd677d1fff1
Shoaib Burq <saburq@gmail.com>
2011-07-01 10:38:15 +0200

    some reason this helper is syntax erroring on production.

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -7,10 +7,11 @@ module ArticlesHelper
           link_to_icon('show', t(:show), m),
           (can? :update,  m) ? link_to_icon('edit', t(:edit), send("edit_#{m.class.name.downcase}_path", m)) : nil,
           (can? :destroy, m) ? link_to_icon('destroy', t(:destroy), m, { :confirm => t(:are_you_sure), :method => :delete }) : nil,
-          m.published? ?
+          if m.published?
             (can? :unpublish, m) ? link_to(t(:unpublish), unpublish_article_path(m), { :confirm => t(:are_you_sure), :method => :put }) : nil
-            :
-            "draft"
+          else
+            t('draft')
+          end
         ].compact.join(' ').html_safe
       end
     end

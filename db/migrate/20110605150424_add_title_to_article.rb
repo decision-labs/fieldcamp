b66f175d0683c3c4d64442ed7f11999e7b637811
Shoaib Burq <saburq@gmail.com>
2011-07-01 16:53:52 +0200

    merged in master - phew!

--- db/migrate/20110605150424_add_title_to_article.rb
+++
@@ -1,9 +0,0 @@
-class AddTitleToArticle < ActiveRecord::Migration
-  def self.up
-    add_column :articles, :title, :string
-  end
-
-  def self.down
-    remove_column :articles, :title
-  end
-end

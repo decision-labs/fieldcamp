9ba19a41e4b72eae0e3a9b54221750a4d8aeddee
Shoaib Burq <saburq@gmail.com>
2011-07-11 20:36:45 +0200

    fixed dashboard events were in reverse chronological order.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -4,7 +4,7 @@ class Admin::DashboardController < ApplicationController
     # @events = current_user.events_since_login.page(params[:page]).per(5).group_by(&:project_id)
     # @has_more_events = current_user.events_since_login.count > 5
 
-    @events = current_user.events_since_login.page(params[:page]).per(5)
+    @events = current_user.events_since_login.order('created_at DESC').page(params[:page]).per(5)
     @projects = Project.where(:id => @events.map(&:project_id).uniq)
 
     # TODO: move to models

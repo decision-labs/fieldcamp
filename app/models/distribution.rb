6ac7653b23dce30fc373a7e3f73f5a815645dcd2
Shoaib Burq <saburq@gmail.com>
2011-07-25 20:07:34 +0200

    Added distribution to event display template

--- app/models/distribution.rb
+++ app/models/distribution.rb
@@ -5,4 +5,16 @@ class Distribution < ActiveRecord::Base
   validates :quantity_of_items, :numericality => true
   validates :number_of_recipients, :numericality => true
   validates :event_id, :numericality => true
+
+  attr_reader :description
+
+  def description
+    str = [self.quantity_of_items, self.unit, self.item].join(" ")
+    unless self.recipient.blank?
+      str = "#{str} #{I18n.t('provided_to')}"
+      str = "#{str} #{self.number_of_recipients}" unless self.number_of_recipients.blank?
+      str = "#{str} #{self.recipient}"
+    end
+  end
+
 end

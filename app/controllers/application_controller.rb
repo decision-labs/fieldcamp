68086669ff9349aa9a03f90d3c3d11b87f1b6784
Shoaib Burq <saburq@gmail.com>
2012-01-30 21:35:47 +0100

    Added basic breadcrumbs.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
   before_filter :set_locale, :prepare_for_mobile
   before_filter :authenticate_user!
   before_filter :set_location_scope
+  before_filter :setup_breadcrumbs, :only => [:index, :show, :edit, :new]
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => t(:unauthorized, :action => t(exception.action), :subject => t(exception.subject))
@@ -22,6 +23,13 @@ class ApplicationController < ActionController::Base
   end
 
   private
+
+  def setup_breadcrumbs
+    add_crumb "Dashboard", '/'
+    add_crumb(self.controller_name.titleize, send("#{self.controller_name}_path") )
+    add_crumb self.action_name.to_s
+  end
+
   def mobile_device?
     return false if request.format.to_s =~ /application\/json/i
     request.user_agent =~ /Mobile|webOS/

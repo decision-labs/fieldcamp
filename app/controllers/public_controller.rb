3c8deefc361b0ced35cdceefb223d515698ba8ab
Shoaib Burq <saburq@gmail.com>
2012-01-30 14:43:39 +0100

    updated routes to direct to dashboard instead of public.

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,6 +1,7 @@
 class PublicController < ApplicationController
   skip_filter :authenticate_user!
   def index
-    @articles = Article.published.page(params[:page]).per(5)
+    # @articles = Article.published.page(params[:page]).per(5)
+    # redirect sign_in
   end
 end

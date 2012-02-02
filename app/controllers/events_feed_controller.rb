83221e559e5cc3495f0f701c9f5f292e172b08f3
Shoaib Burq <saburq@gmail.com>
2012-02-02 19:35:07 +0100

    Using the helper.

--- app/controllers/events_feed_controller.rb
+++ app/controllers/events_feed_controller.rb
@@ -2,9 +2,9 @@ class EventsFeedController < ApplicationController
   def index
 
     #FIXME: Dry this branching
-    unless @current_user_location_ids.blank?
+    unless current_user_location_ids.blank?
       @events = Event.joins(:project).where(
-        'projects.location_id' => @current_user_location_ids
+        'projects.location_id' => current_user_location_ids
       ).order('created_at desc').page(params[:page]).per(5)
     else
       @events = Event.joins(:project).order('created_at desc').page(params[:page]).per(5)
